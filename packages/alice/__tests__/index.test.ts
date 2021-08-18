40a9e076952635547a35315d5a8da38a7bcf47f6
Alphability <albanmezino@gmail.com>
2021-08-18 06:30:22 +0200

    🐛 ci no require

--- packages/alice/__tests__/index.test.ts
+++ packages/alice/__tests__/index.test.ts
@@ -1,61 +1,54 @@
-import { join } from 'path';
+beforeEach(() => {
+  jest.resetModules();
+});
 
 describe('Success cases', () => {
-  test('It should return a scrollTop value different from 0', () => {
-    jest.isolateModules(() => {
-      // Change the scroll value to 1000px.
-      global.scrollY = 1000;
+  test('It should return a scrollTop value different from 0', async () => {
+    // Change the scroll value to 1000px.
+    global.scrollY = 1000;
 
-      // eslint-disable-next-line
-      const { Alice } = require(join(__dirname, '../src/index'));
-      const alice = new Alice();
-      alice.initialize();
+    const { Alice } = await import('../src/index');
+    const alice = new Alice();
+    alice.initialize();
 
-      // Trigger the window scroll event.
-      global.dispatchEvent(new Event('scroll'));
-      global.dispatchEvent(new Event('scroll'));
+    // Trigger the window scroll event.
+    global.dispatchEvent(new Event('scroll'));
+    global.dispatchEvent(new Event('scroll'));
 
-      expect(alice.scroll.data.scrollTop).toStrictEqual(1000);
+    expect(alice.scroll.data.scrollTop).toStrictEqual(1000);
 
-      alice.destroy();
-    });
+    alice.destroy();
   });
 
-  test('It should not take the first scroll event into consideration', () => {
-    jest.isolateModules(() => {
-      // Change the scroll value to 1000px.
-      global.scrollY = 1000;
+  test('It should not take the first scroll event into consideration', async () => {
+    // Change the scroll value to 1000px.
+    global.scrollY = 1000;
 
-      // eslint-disable-next-line
-      const { Alice } = require(join(__dirname, '../src/index'));
-      const alice = new Alice();
-      alice.initialize();
+    const { Alice } = await import('../src/index');
+    const alice = new Alice();
+    alice.initialize();
 
-      // Triggering the window's first scroll event only.
-      global.dispatchEvent(new Event('scroll'));
+    // Triggering the window's first scroll event only.
+    global.dispatchEvent(new Event('scroll'));
 
-      expect(alice.scroll.data.scrollTop).toStrictEqual(0);
+    expect(alice.scroll.data.scrollTop).toStrictEqual(0);
 
-      alice.destroy();
-    });
+    alice.destroy();
   });
 
-  test('It should not take the scroll event into consideration', () => {
-    jest.isolateModules(() => {
-      // Change the scroll value to 1000px.
-      global.scrollY = 1000;
+  test('It should not take the scroll event into consideration', async () => {
+    // Change the scroll value to 1000px.
+    global.scrollY = 1000;
 
-      // eslint-disable-next-line
-      const { Alice } = require(join(__dirname, '../src/index'));
-      const alice = new Alice();
-      alice.initialize();
-      alice.destroy();
+    const { Alice } = await import('../src/index');
+    const alice = new Alice();
+    alice.initialize();
+    alice.destroy();
 
-      // Trigger the window scroll event.
-      global.dispatchEvent(new Event('scroll'));
-      global.dispatchEvent(new Event('scroll'));
+    // Trigger the window scroll event.
+    global.dispatchEvent(new Event('scroll'));
+    global.dispatchEvent(new Event('scroll'));
 
-      expect(alice.scroll.data.scrollTop).toStrictEqual(0);
-    });
+    expect(alice.scroll.data.scrollTop).toStrictEqual(0);
   });
 });
